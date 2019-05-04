462e27fb1f570f8b799d2e41f420bee0974d44b3
Sadra <sadra.z.kh@gmail.com>
2019-05-04 20:08:23 +0430

    Back#0.5

---
+++ AP_Project/AP_Project/Back-End/Modals/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AP_Project.Back_End.Modals
+{
+    public class Context : DbContext
+    {
+        public DbSet<Persons.Person> Persons { get; set; }
+        public DbSet<Products.Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=E:\GitHub\AP_Project\AP_Project\AP_Project\AP_Project_DB;Trusted_Connection=True;");
+        }
+    }
+}
