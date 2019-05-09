e0ab8603c52385a0a79b489272e8dae72845b057
Sadra <sadra.z.kh@gmail.com>
2019-05-10 02:41:33 +0430

    back1.11

--- AP_Project/AP_Project/Back-End/Modals/Products/Product.cs
+++ AP_Project/AP_Project/Back-End/Modals/Products/Product.cs
@@ -9,6 +9,7 @@ namespace AP_Project.Back_End.Modals.Products
     public class Product
     {
         public int ProductId { get; set; }
+        public int ProductBarcode { get; set; }
         public string ProductName { get; set; }
         public string ProductPhotoAdress { get; set; }
         public int Store { get; set; }
